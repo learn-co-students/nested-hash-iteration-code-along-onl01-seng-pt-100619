850fe363e9bb6d826fa8cdc57f4b3ced44eee8aa
Ricky dildy <rdildy92@gmail.com>
2019-11-06 18:20:05 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,23 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, data|
+     # contacts = {
+  #   "Jon Snow" => {
+  #     name: "Jon",
+  #     email: "jon_snow@thewall.we", 
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #   },
+  
+  #   "Freddy Mercury" => {
+  #     name: "Freddy",
+  #     email: "freddy@mercury.com",
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
+   contacts("Freddy Mercury").each |favorite_ice_cream_flavors| do 
+     favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
+     binding.pry 
+    end 
+        end 
 end
